93c49f2850b19015675870b2d7bac64eaedd8cac
xplainr <3811651+xplainr@users.noreply.github.com>
2025-04-30 23:06:54 -0700

    Revert: switch @components alias to relative import due to unresolved path in .astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-      resolve: {
-        alias: {
-          '@components': new URL('./src/components', import.meta.url).pathname
-        }
+  vite: {
+    resolve: {
+      alias: {
+        '@components': new URL('./src/components', import.meta.url).pathname
       }
     }
-  });
+  }
+});
