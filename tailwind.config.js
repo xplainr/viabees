2105b64e8b54a5841d92ff4a632832831fb124a4
xplainr <3811651+xplainr@users.noreply.github.com>
2025-05-01 09:12:36 -0700

    Style: set homepage background to #fbf3e4 via Tailwind custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: '#fbf3e4'
+      }
+    }
   },
   plugins: []
 };
